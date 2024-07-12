0018c615175bbcfa939447dd28db5f5db3408074
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2024-07-12 10:41:46 +0530

    Add files via upload

---
+++ react-intro/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React</h1>
+      <p>React is a JavaScript library for building user interfaces.</p>
+      <p>React is used to build single-page applications.</p>
+      <p>React allows us to create reusable UI components.</p>
+      <img src="https://media.licdn.com/dms/image/D5612AQE7A9az3VXM0Q/article-cover_image-shrink_600_2000/0/1667398918313?e=2147483647&v=beta&t=z1G4bq8s7emskXcbdcKnfMhNflHHBjtxKek9fBPrgTU" alt="React Image" />\
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
